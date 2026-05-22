a42ad12fe4e20562220fcd5a2ceb3387dbae4f55
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-05-22 00:55:20 -0500

    Time: 2 ms (88.54%), Space: 58.7 MB (84.98%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.ts
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.ts
@@ -1,44 +1,39 @@
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-
-    let small = nums1;
-    let large = nums2;
+    let total = nums1.length + nums2.length
+    let half = Math.floor(total / 2)
+    let small = nums1
+    let large = nums2
 
     if (nums1.length > nums2.length) {
-        [small, large] = [large, small];
+        [small, large] = [large, small]
     }
 
-    let total = small.length + large.length;
-    let half = Math.floor(total / 2);
-
-    let l = 0;
-    let r = small.length;
+    let sl = 0
+    let sr = small.length - 1
 
     while (true) {
+        let smid = Math.floor((sl + sr) / 2) 
+        let lmid = half - smid - 2
 
-        let mid1 = Math.floor((l + r) / 2);
-        let mid2 = half - mid1;
-
-        let sleft = mid1 > 0 ? small[mid1 - 1] : -Infinity;
-        let sright = mid1 < small.length ? small[mid1] : Infinity;
-
-        let lleft = mid2 > 0 ? large[mid2 - 1] : -Infinity;
-        let lright = mid2 < large.length ? large[mid2] : Infinity;
+        let sleft = smid >= 0 ? small[smid] : -Infinity
+        let sright = ((smid + 1) < (small.length )) ? small[smid + 1] : Infinity
+        let lleft = lmid >= 0 ? large[lmid] : -Infinity
+        let lright = (lmid + 1) < (large.length ) ? large[lmid + 1] : Infinity
 
         if (sleft <= lright && lleft <= sright) {
-
-            if (total % 2) {
-                return Math.min(sright, lright);
+            if ((total % 2) != 0) {
+                return Math.min(sright, lright)
+            } else {
+                return (Math.max(sleft, lleft) + Math.min(lright, sright)) / 2
             }
-
-            return (
-                Math.max(sleft, lleft) +
-                Math.min(sright, lright)
-            ) / 2;
-
-        } else if (sleft > lright) {
-            r = mid1 - 1;
-        } else {
-            l = mid1 + 1;
+        }else{
+            if(sleft > lright){
+                sr = smid - 1
+            }else{
+                sl = smid + 1
+            }
         }
     }
-}
+    return 0
+
+};
