d218dc5169a4080f9be1fa18dd80a98e85dde2f1
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-06-11 19:45:26 -0500

    Time: 3 ms (93.42%), Space: 58.2 MB (82.89%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.ts
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.ts
@@ -2,17 +2,6 @@ function maxProfit(k: number, prices: number[]): number {
     const n = prices.length;
     if (n === 0) return 0;
 
-    // if k is huge, same as unlimited transactions
-    if (k >= Math.floor(n / 2)) {
-        let profit = 0;
-        for (let i = 1; i < n; i++) {
-            if (prices[i] > prices[i - 1]) {
-                profit += prices[i] - prices[i - 1];
-            }
-        }
-        return profit;
-    }
-
     const buy = new Array(k + 1).fill(-Infinity);
     const sell = new Array(k + 1).fill(0);
 
