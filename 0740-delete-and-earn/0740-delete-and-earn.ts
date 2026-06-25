80d281492b981cab9a350e9296aea68178c7a300
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-06-24 20:30:19 -0500

    Time: 7 ms (73.68%), Space: 57.6 MB (96.49%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.ts
+++ 0740-delete-and-earn/0740-delete-and-earn.ts
@@ -1,39 +1,19 @@
 function deleteAndEarn(nums: number[]): number {
-    let freq = new Map<number, number>
+    let max = Math.max(...nums)
+    let dp = new Array(max + 1).fill(0)
 
-    for (let i = 0; i < nums.length; i++) {
-        let val = freq.get(nums[i]) || 0
-        freq.set(nums[i], val + 1)
+    for (let num of nums) {
+        dp[num] += num
     }
-
-    let keys = Array.from(freq.keys()).sort((a, b) => a - b)
-    let memo = new Map<number, number>()
-    let max = 0
-    function traversal(index) {
-        if (index >= keys.length) {
-            return 0
-        }
-
-        let current: number = keys[index]
-        if (memo.has(current)) {
-            return memo.get(current)
-        }
-
-
-        let localScore = current * freq.get(current)
-        let keep = traversal(index + 1)
-        let next = index + 1
-        if (index + 1 < keys.length && keys[index + 1] == current + 1) {
-            next = index + 2
-        }
-        let deletes = localScore + traversal(next)
-        let res = Math.max(keep, deletes)
-
-        memo.set(current, res)
-        return res
-
+    let takePrev = dp[0]
+    let skipPrev = 0
+    for (let i = 0; i <= max; i++) {
+        let currentScore = Math.max(takePrev, skipPrev + dp[i])
+        dp[i] = currentScore
+       
+        skipPrev = takePrev
+        takePrev = currentScore
     }
-
-    return traversal(0)
-
+    console.log(dp)
+    return dp[max]
 };
