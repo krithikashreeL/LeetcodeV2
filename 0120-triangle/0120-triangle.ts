5a9a93419d276eafbe032f57826d1ec1d7902070
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-06-25 22:37:09 -0500

    Time: 18 ms (5.03%), Space: 59.7 MB (16.98%) - LeetHub

---
+++ 0120-triangle/0120-triangle.ts
@@ -0,0 +1,20 @@
+function minimumTotal(triangle: number[][]): number {
+    let min = 0
+    let m = triangle.length
+
+    for (let i = m - 1; i > 0; i--) {
+        let prev = triangle[i - 1]
+        let current = triangle[i]
+        let prevLength = prev.length
+
+        for(let i = 0; i < prevLength; i++){
+            prev[i] += Math.min(current[i], current[i+1])
+        }
+        
+    }
+    console.log(triangle)
+
+    return triangle[0][0]
+};
+
+
