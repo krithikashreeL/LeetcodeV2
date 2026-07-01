ebb720c3efd98f298cf6d56e008e5445373c8e9c
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-06-30 20:37:34 -0500

    Time: 57 ms (14.2%), Space: 68.5 MB (14.2%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.ts
+++ 0221-maximal-square/0221-maximal-square.ts
@@ -1,40 +1,30 @@
 function maximalSquare(matrix: string[][]): number {
-    let max = 0
+
+    let dp = []
     let m = matrix.length
     let n = matrix[0].length
 
-    let dp = []
-    for (let i = 0; i < m; i++) {
+
+    for(let i = 0; i < m; i++){
         dp.push(new Array(n).fill(0))
     }
-    // console.log(dp)
+    let max = 0
 
     for (let i = m - 1; i >= 0; i--) {
         for (let j = n - 1; j >= 0; j--) {
-            if (matrix[i][j] == '0') {
-                dp[i][j] = '0'
-            } else {
-                let down = 0
-                let right = 0
-                let diagonal = 0
 
-                if ((i + 1) >= 0 && (i + 1) < m) {
-                    down = dp[i + 1][j]
-                }
-                if ((j + 1) < n && (j + 1) >= 0) {
-                    right = dp[i][j + 1]
-                }
-                if ((j + 1) >= 0 && (j + 1) < n && (i + 1) >= 0 && (i + 1) < m) {
-                    diagonal = dp[i + 1][j + 1]
-                }
-
-                dp[i][j] = 1 + Math.min(diagonal, down, right)
-                max = Math.max(dp[i][j],max)
-
-
+            let current = matrix[i][j]
+            if (current == '1') {
+                let right = j + 1 < n ? dp[i][j + 1] : 0
+                let bottom = i + 1 < m ? dp[i + 1][j] : 0
+                let diag =  i + 1 < m && j + 1 < n ? dp[i + 1][j + 1] : 0
+                dp[i][j] = 1 + Math.min(right, bottom, diag)
+                max = Math.max(max,dp[i][j])
             }
         }
     }
+
+    console.log(dp)
 
     return max * max
 };
