4d6931d2ae583a78b90cbbd78567df78b5eefa6f
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-07-01 21:13:24 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0139-word-break/0139-word-break.ts
+++ 0139-word-break/0139-word-break.ts
@@ -1,50 +1,20 @@
 function wordBreak(s: string, wordDict: string[]): boolean {
-
-    class TrieNode {
-        map = new Map<string, TrieNode>();
-        isEnd = false;
-    }
-
-    const root = new TrieNode();
-
-    for (const word of wordDict) {
-        let node = root;
-        for (const c of word) {
-            if (!node.map.has(c)) {
-                node.map.set(c, new TrieNode());
-            }
-            node = node.map.get(c)!;
+    let memo = new Map<string, boolean>()
+    function dfs(s) {
+        if (s == '') {
+            return true
         }
-        node.isEnd = true;
-    }
-
-    const memo = new Map<number, boolean>();
-
-    function dfs(index: number): boolean {
-        if (index === s.length) return true;
-
-        if (memo.has(index)) return memo.get(index)!;
-
-        let node = root;
-
-        for (let i = index; i < s.length; i++) {
-            const c = s[i];
-
-            if (!node.map.has(c)) break;
-
-            node = node.map.get(c)!;
-
-            if (node.isEnd) {
-                if (dfs(i + 1)) {
-                    memo.set(index, true);
-                    return true;
+        for (const word of wordDict) {
+            if (s.startsWith(word)) {
+                if (dfs(s.slice(word.length))) {
+                    memo.set(s, true)
+                    return true
                 }
             }
         }
-
-        memo.set(index, false);
-        return false;
+        memo.set(s, false)
+        return false
     }
 
-    return dfs(0);
-}
+    return dfs(s)
+};
