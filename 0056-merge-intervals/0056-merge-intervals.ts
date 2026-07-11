3d4bd7113e7e09f808022fd9dbb45808fdb1de98
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-07-10 19:53:42 -0500

    Time: 19 ms (13.25%), Space: 68.2 MB (6.9%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.ts
@@ -0,0 +1,21 @@
+function merge(intervals: number[][]): number[][] {
+    intervals = intervals.sort((a,b) => a[0] - b[0])
+    let results= []
+    let n = intervals.length
+    let start = intervals[0][0]
+    let end = intervals[0][1]
+    for (let i = 1; i < n; i++) {
+        let [nextS, nextE] = intervals[i]
+        if (nextS > end) {
+            results.push([start, end])
+            start = nextS
+            end = Math.max(end,nextE)
+        } else {
+            end = Math.max(end,nextE)
+        }
+    }
+    results.push([start,end])
+
+    console.log(results)
+    return results
+};
