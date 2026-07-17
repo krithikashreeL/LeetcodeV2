93f6297592bbf6be2c4592a052f2ec1d37708902
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-07-16 19:48:55 -0500

    Time: 16 ms (5.15%), Space: 58.7 MB (10.82%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.ts
@@ -0,0 +1,31 @@
+function generateParenthesis(n: number): string[] {
+
+    let results = []
+
+
+    function dfs(str, index, close) {
+        if (str.length == n * 2 ) {
+            if(index == close){
+            console.log(str)
+            results.push(str)
+            }
+            return
+        }
+
+        // add close
+        if (close < index) {
+            let str2 = str + ')'
+             dfs(str2, index , close + 1)
+        }
+        // add open
+        let str3 = str + '('
+        dfs(str3, index + 1, close)
+
+
+        return
+    }
+
+    dfs('(', 1 , 0)
+
+    return results
+};
