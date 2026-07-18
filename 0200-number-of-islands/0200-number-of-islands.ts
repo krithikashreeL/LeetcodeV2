f230c9a887ee05265b384d031e867be21a539fa4
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-07-18 01:14:50 -0500

    Time: 56 ms (85.41%), Space: 59 MB (77.99%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.ts
+++ 0200-number-of-islands/0200-number-of-islands.ts
@@ -2,18 +2,18 @@ function numIslands(grid: string[][]): number {
 
     let m = grid.length
     let n = grid[0].length
-    let seen = new Set()
+    // let seen = new Set()
     function dfs(r, c, counter) {
         if (r >= m || c >= n || r < 0 || c < 0) {
             return
         }
-        let key = String(r) + '|' + String(c)
-        if (grid[r][c] == '0' || seen.has(key)) {
+        // let key = String(r) + '|' + String(c)
+        if (grid[r][c] == '0') {
             return
         }
         
-        seen.add(key)
-        grid[r][c] = String(counter)
+        // seen.add(key)
+        grid[r][c] = '0'
 
         dfs(r + 1, c, counter)
         dfs(r, c + 1, counter)
@@ -25,8 +25,8 @@ function numIslands(grid: string[][]): number {
     let counter = 0
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            let key = String(i) + '|' + String(j)
-            if (grid[i][j] == '1' && !seen.has(key)) {
+            // let key = String(i) + '|' + String(j)
+            if (grid[i][j] == '1' ) {
                  counter += 1
                 dfs(i,j,counter)
                
