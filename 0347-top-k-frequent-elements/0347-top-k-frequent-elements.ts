b14f107594fa2db3ad65d3205948b73fdf8b0a8e
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-07-17 23:21:24 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.ts
@@ -0,0 +1,58 @@
+function topKFrequent(nums: number[], k: number): number[] {
+
+    let map = new Map<number, number>()
+    let n = nums.length
+
+    for (let i = 0; i < n; i++) {
+        let val = map.get(nums[i]) ?? 0
+        map.set(nums[i], val + 1)
+    }
+    let array = Array.from(map)
+    // console.log(array)
+    function maxHeap(parent, arr) {
+        let n = arr.length
+        let left = parent * 2 + 1
+        let right = parent * 2 + 2
+        let largest = parent
+
+        if (left >= 0 && left < n && arr[left][1] > arr[largest][1]) {
+            largest = left
+        }
+
+        if (right >= 0 && right < n && arr[right][1] > arr[largest][1]) {
+            largest = right
+        }
+
+        if (largest !== parent) {
+            [arr[largest], arr[parent]] = [arr[parent], arr[largest]]
+            maxHeap(largest, arr)
+        }
+
+        return
+    }
+
+    function heapify(arr) {
+        let lastParent = Math.floor(arr.length / 2) - 1
+
+        for (let i = lastParent; i >= 0; i--) {
+            maxHeap(i, arr)
+        }
+
+        return arr
+    }
+
+
+
+
+    let max = []
+    while (k > 0 && array.length > 0) {
+        array = heapify(array)
+        let top = array[0]
+        max.push(top[0])
+        array[0] = array[array.length - 1]
+        array.pop()
+        k-=1
+    }
+    return max
+};
+
