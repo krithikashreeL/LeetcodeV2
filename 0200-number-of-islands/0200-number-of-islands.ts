9d4216ff98e1a3bb6fc2b7095b3b955f42ff9311
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-07-18 01:01:45 -0500

    Time: 101 ms (15.18%), Space: 71 MB (6.49%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.ts
@@ -0,0 +1,41 @@
+function numIslands(grid: string[][]): number {
+
+    let m = grid.length
+    let n = grid[0].length
+    let seen = new Set()
+    function dfs(r, c, counter) {
+        if (r >= m || c >= n || r < 0 || c < 0) {
+            return
+        }
+        let key = String(r) + '|' + String(c)
+        if (grid[r][c] == '0' || seen.has(key)) {
+            return
+        }
+        
+        seen.add(key)
+        grid[r][c] = String(counter)
+
+        dfs(r + 1, c, counter)
+        dfs(r, c + 1, counter)
+        dfs(r - 1, c , counter)
+        dfs(r,c - 1, counter)
+        return
+    }
+
+    let counter = 0
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            let key = String(i) + '|' + String(j)
+            if (grid[i][j] == '1' && !seen.has(key)) {
+                 counter += 1
+                dfs(i,j,counter)
+               
+            }
+            
+        }
+    }
+
+    console.log(grid)
+
+    return counter
+};
