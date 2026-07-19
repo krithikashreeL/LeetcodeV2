e59fdf209bd7acd5136e5e0df38c580b73c5162d
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-07-18 21:27:20 -0500

    Time: 0 ms (100%), Space: 55.9 MB (7.17%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.ts
@@ -0,0 +1,20 @@
+function rob(nums: number[]): number {
+    let n = nums.length
+    let dp = new Array(n).fill(0)
+    if (n <= 2) {
+        return Math.max(...nums)
+    }
+    dp[0] = nums[0]
+    dp[1] = Math.max(nums[0],nums[1])
+    for (let i = 2; i < n; i++) {
+        
+        // rob
+        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i])
+    }
+
+
+    return dp[n - 1]
+
+};
+
+
