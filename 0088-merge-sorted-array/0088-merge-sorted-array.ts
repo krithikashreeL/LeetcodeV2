207cff9932d1bc433c15ba2c7dba44fd8b69a6ce
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-07-19 13:49:49 -0500

    Time: 0 ms (100%), Space: 56.4 MB (26.84%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.ts
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -1,21 +1,19 @@
-/**
- Do not return anything, modify nums1 in-place instead.
- */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    let start = 0
-    for(let i = m; i < nums1.length; i++){
-        nums1[i] = nums2[start]
-        start += 1
-    }
-    console.log(nums1)
-    for(let i = 0; i < m + n; i++){
-        for(let j = m + n; j >= i; j--){
-            if(nums1[i] > nums1[j]){
-                // console.log(nums1[i],nums1[j])
-                let temp = nums1[j]
-                nums1[j] = nums1[i]
-                nums1[i] = temp
-            }
+    let position = m + n - 1;
+    m -= 1;
+    n -= 1;
+
+    // We only need to loop as long as there are items left in nums2 to place.
+    // If nums2 runs out, whatever is left in nums1 is already sorted!
+    while (n >= 0) {
+        if (m >= 0 && nums1[m] > nums2[n]) {
+            nums1[position] = nums1[m];
+            m -= 1;
+        } else {
+            // This runs if nums2[n] >= nums1[m] OR if nums1 is exhausted (m < 0)
+            nums1[position] = nums2[n];
+            n -= 1;
         }
+        position -= 1;
     }
-};
+}
