f3845e55325792b6f05dd5da70242e7d6ba7799b
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-07-21 21:36:36 -0500

    Time: 252 ms (5.01%), Space: 88.1 MB (6.37%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.ts
@@ -0,0 +1,24 @@
+function longestConsecutive(nums: number[]): number {
+    if (nums.length == 0) {
+        return 0
+    }
+    nums.sort((a, b) => a - b)
+    let set = new Set(nums)
+    let max = 1
+    let n = nums.length
+    for (let i = 0; i < n; i++) {
+        let start = nums[i] 
+        while (set.has(start + 1)) {
+            start += 1
+        }
+        console.log(nums[i], start)
+        let diff = start - nums[i] + 1
+        if (diff > 0) {
+            i += diff - 1
+        }
+        max = Math.max(diff, max)
+    }
+
+    return max
+
+};
