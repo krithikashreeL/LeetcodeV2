4edf70796ad4700d923645b037c5cd1c2d2a0c63
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-07-23 21:28:25 -0500

    Time: 100 ms (31.36%), Space: 64.2 MB (32.02%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.ts
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.ts
@@ -1,82 +1,46 @@
-// /**
-//  * Definition for singly-linked list.
-//  * class ListNode {
-//  *     val: number
-//  *     next: ListNode | null
-//  *     constructor(val?: number, next?: ListNode | null) {
-//  *         this.val = (val===undefined ? 0 : val)
-//  *         this.next = (next===undefined ? null : next)
-//  *     }
-//  * }
-//  */
-
-// function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
-
-
-
-//     let result = new ListNode(0)
-//     console.log(lists.length)
-//     if (lists.length == 0) {
-//         return null
-//     }
-//     let resultHead: ListNode | null = result
-
-//     while (lists.length > 0) {
-//         let minIndex = 0;
-//         for (let i = 1; i < lists.length; i++) {
-//             if (lists[minIndex].val > lists[i].val) {
-//                 minIndex = i
-//             }
-//         }
-
-//         let smallest = lists[minIndex]
-//         result.next = smallest
-//         result = result.next
-//         if (smallest !== null && smallest.next !== null) {
-//             lists[minIndex] = lists[minIndex].next
-
-//         } else {
-//             lists.splice(minIndex, 1)
-//         }
-
-//     }
-//     console.log(resultHead.next)
-
-//     return resultHead.next
-// };
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
 
 function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
-    // 1. Clean out all null lists up front so lists[i] is guaranteed to be a ListNode
+
+
     lists = lists.filter((node): node is ListNode => node !== null);
-
-    if (lists.length === 0) return null;
-
-    const dummy = new ListNode(0);
-    let current = dummy;
+    let result = new ListNode(0)
+    console.log(lists.length)
+    if (lists.length == 0) {
+        return null
+    }
+    let resultHead: ListNode | null = result
 
     while (lists.length > 0) {
         let minIndex = 0;
-
-        // 2. Find index of the node with the smallest value
         for (let i = 1; i < lists.length; i++) {
-            if (lists[i].val < lists[minIndex].val) {
-                minIndex = i;
+            if (lists[minIndex].val > lists[i].val) {
+                minIndex = i
             }
         }
 
-        const smallest = lists[minIndex];
+        let smallest = lists[minIndex]
+        result.next = smallest
+        result = result.next
+        if (smallest !== null && smallest.next !== null) {
+            lists[minIndex] = lists[minIndex].next
 
-        // 3. Attach the SMALLEST node directly (no need for `new ListNode`)
-        current.next = smallest;
-        current = current.next;
-
-        // 4. Advance that list's head, or remove it from `lists` if finished
-        if (smallest.next !== null) {
-            lists[minIndex] = smallest.next;
         } else {
-            lists.splice(minIndex, 1);
+            lists.splice(minIndex, 1)
         }
-    }
 
-    return dummy.next;
-}
+    }
+    console.log(resultHead.next)
+
+    return resultHead.next
+};
