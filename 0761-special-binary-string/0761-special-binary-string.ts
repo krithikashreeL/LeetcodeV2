c832d9e818f1c95026571ce463d6af6baf3f54b3
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-07-23 20:16:24 -0500

    Time: 0 ms (100%), Space: 56.4 MB (95.65%) - LeetHub

--- 0761-special-binary-string/0761-special-binary-string.ts
+++ 0761-special-binary-string/0761-special-binary-string.ts
@@ -1,23 +1,29 @@
 function makeLargestSpecial(s: string): string {
+
+    //   11011000
+    //   // condition 1
+    //   1 = 4
+    //   0 = 4
+    //   // condition 
+    //   1,0 2,0 2,1 3,1 4,1 4,2 4,3 4,4
+
+    let result :string[] = []
     let balance = 0
-    let results: string[] = []
     let start = 0
     for (let i = 0; i < s.length; i++) {
-
         if (s[i] == '1') {
             balance += 1
         } else {
             balance -= 1
         }
+
         if (balance == 0) {
-            // always starts at 1, ends at 0
             let inner = s.slice(start + 1, i)
-            results.push('1' + makeLargestSpecial(inner) + '0')
+            result.push('1' + makeLargestSpecial(inner) + '0')
             start = i + 1
         }
-
     }
 
-    results.sort((a, b) => b > a ? 1 : b < a ? -1 : 0)
-    return results.join('')
+    result.sort((a, b) => b > a ? 1 : b < a ? -1 : 0)
+    return result.join('')
 };
