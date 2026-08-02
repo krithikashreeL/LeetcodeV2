7b68a907fb10e0a04294f020385ae7e94846558f
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-08-01 22:31:25 -0500

    Time: 51 ms (54.31%), Space: 83.9 MB (22.85%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.ts
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.ts
@@ -1,26 +1,29 @@
 function longestCommonSubsequence(text1: string, text2: string): number {
-    let matrix: number[][] = Array.from(
-        { length: text1.length },
-        () => new Array(text2.length).fill(0)
-    );
-    let max = 0;
+    let m = text1.length
+    let n = text2.length
 
-    for (let i = 0; i < text1.length; i++) {
-        for (let j = 0; j < text2.length; j++) {
-            if (text1[i] === text2[j]) {
-                // Characters match -> extend from diagonal
-                let diagonal = (i > 0 && j > 0) ? matrix[i - 1][j - 1] : 0;
-                matrix[i][j] = diagonal + 1;
-            } else {
-                // Characters don't match -> carry over best result from left or top
-                let top = i > 0 ? matrix[i - 1][j] : 0;
-                let left = j > 0 ? matrix[i][j - 1] : 0;
-                matrix[i][j] = Math.max(top, left);
+    let dp = []
+    for (let i = 0; i < m; i++) {
+        dp.push(new Array(n).fill(0))
+    }
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            let top = i - 1 >= 0 ? dp[i - 1][j] : 0
+            let left = j - 1 >= 0 ? dp[i][j - 1] : 0
+            let diagonal = i - 1 >= 0 && j - 1 >= 0 ? dp[i - 1][j - 1] : 0
+            let current = 0
+            if (text1[i] == text2[j]) {
+                 dp[i][j] = 1 + diagonal
+            }else{
+                dp[i][j] = Math.max(top,left)
             }
-            
-            max = Math.max(max, matrix[i][j]);
+            // console.log(text1[i], text2[j])
+           
         }
     }
 
-    return max;
-}
+    // console.log(dp)
+
+    return dp[m-1][n-1]
+};
