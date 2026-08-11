27c5201b0eab95e2fd515341de86ee3df05871f1
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-08-10 20:20:29 -0500

    Time: 8 ms (89.47%), Space: 59.5 MB (89.47%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.ts
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.ts
@@ -1,34 +1,18 @@
 function findLongestChain(pairs: number[][]): number {
-    let dp = new Array(pairs.length).fill(1)
+    // let dp = new Array(pairs.length).fill(1)
 
     pairs.sort((a, b) => a[1] - b[1])
     let ans = 1
+    let count = 1
+    let end = pairs[0][1]
     for (let i = 1; i < pairs.length; i++) {
-        for (let j = 0; j < i; j++) {
-            if (pairs[j][1] < pairs[i][0]) {
-                dp[i] = Math.max(dp[i], dp[j] + 1)
-            }
+        let start = pairs[i][0]
+        if (end < start) {
+            count += 1
+            end = pairs[i][1]
         }
-        ans = Math.max(ans, dp[i])
+        ans = Math.max(ans, count)
     }
 
     return ans
-    // let max = 0
-    // for(let i = 0; i < pairs.length; i++){
-    //     let start = pairs[i][1]
-    //     let count = 1
-    //     for(let j = i + 1; j < pairs.length; j++){
-    //         let end = pairs[j][0]
-    //         // console.log(start,end)
-    //         if(end > start){
-    //             count += 1
-    //             start = pairs[j][1]
-    //         }
-    //     }
-    //     max = Math.max(count,max)
-
-// }
-
-// return max
-    
 };
