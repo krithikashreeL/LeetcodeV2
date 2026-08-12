6710ca732df529597d85af6473e3948069fa2fd3
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-08-11 20:12:57 -0500

    Time: 81 ms (51.41%), Space: 58.5 MB (17.55%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.ts
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.ts
@@ -1,22 +1,17 @@
 function lengthOfLIS(nums: number[]): number {
-
-    let n = nums.length
-    if(n < 1){
-        return 0
-    }
-    let dp = new Array(n).fill(1)
+    
+    let dp = new Array(nums.length).fill(1)
     let max = 1
-    for (let i = n - 1; i >= 0; i--) {
 
-        for (let j = i; j < n; j++) {
-            if (nums[i] < nums[j]) {
+    for(let i = nums.length - 1; i >= 0; i--){
+        for(let j = i + 1; j < nums.length; j++){
+            if(nums[j] > nums[i]){
                 dp[i] = Math.max(dp[i], dp[j] + 1)
-                max = Math.max(dp[i], max)
             }
         }
+        max = Math.max(max,dp[i])
+
     }
-
-
 
 
     return max
