a10c2162c2343e196a670271ab941a927a51fd68
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-08-18 21:58:02 -0500

    Time: 1 ms (69.57%), Space: 56.2 MB (62.32%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.ts
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.ts
@@ -7,6 +7,6 @@ function maxProfit(prices: number[]): number {
         buy[i] = Math.max(buy[i - 1], (sell[i - 2] ?? 0) - prices[i])
         sell[i] += Math.max(sell[i - 1], buy[i - 1] + prices[i])
     }
-    console.log(sell, buy)
+    // console.log(sell, buy)
     return sell[prices.length - 1]
 };
