5fea726ee4b453b4abc72d8454117f0ce7d2e8fe
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-09-02 21:24:49 -0500

    Time: 0 ms (100%), Space: 57.6 MB (53.85%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.ts
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.ts
@@ -2,21 +2,15 @@ function numTilings(n: number): number {
 
     let mod = 1000000007
 
-    let memo = new Map<number, number>()
-    function dfs(localCount) {
-       if(localCount == 0) return 1
-       if(localCount == 1) return 1
-       if(localCount == 2) return 2
-        if (memo.has(localCount)) {
-            return memo.get(localCount)
-        }
+    let dp = new Array(n + 1).fill(0)
+    dp[0] = 1
+    dp[1] = 1
+    dp[2] = 2
 
-        // add domino
-        let val = (2 * dfs(localCount - 1) + dfs(localCount - 3)) % mod
-        memo.set(localCount, val)
-        return val
+    for(let i = 3; i <= n; i++){
+        dp[i] = (2 * dp[i-1] + dp[i-3] )% mod
     }
 
-    return dfs(n)
+    return dp[n]
     
 };
