d1a03fbea4dc42284c8ce5778d693313017152a2
krithikashreeL <69192489+krithikashreeL@users.noreply.github.com>
2026-09-21 18:18:55 -0600

    Time: 7 ms (3.7%), Space: 59 MB (5.66%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.ts
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.ts
@@ -1,32 +1,33 @@
 function searchRange(nums: number[], target: number): number[] {
 
-    function binarySearch(first: boolean):number {
+
+    function isStartFound(startIndex: Boolean) {
         let start = 0
         let end = nums.length - 1
-        let mid;
         let n = nums.length
         let index = -1
-        // if (n == 1 && nums[0] == target) return [0, 0]
         while (start <= end) {
-            mid = Math.floor((start + end) / 2)
-            if (nums[mid] < target) {
-                start = mid + 1
-            } else if (nums[mid] > target) {
-                end = mid -1
-            } else {
+            let mid = Math.floor((start + end) / 2)
+            console.log(start, end, mid)
+            if (nums[mid] == target) {
                 index = mid
-                if (first) {
-                    end = mid -1
+                if (startIndex) {
+                    end = mid - 1
                 } else {
                     start = mid + 1
                 }
+            } else if (nums[mid] > target) {
+                end = mid - 1
+            } else {
+                start = mid + 1
             }
         }
+
+
         return index
     }
 
+    return [isStartFound(true), isStartFound(false)]
 
-    return [binarySearch(true),binarySearch(false)]
+
 };
-
-
